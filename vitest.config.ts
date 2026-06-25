30b78971869624db20bb94bb51520e887a484cc5
Celtian <dominik.hladik@seznam.cz>
2026-06-25 17:49:25 +0200

    fix(vite): config changed

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,12 @@ import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
 
 export default mergeConfig(
-  viteConfig,
+  viteConfig({
+    command: 'serve',
+    mode: 'test',
+    isPreview: false,
+    isSsrBuild: false,
+  }),
   defineConfig({
     test: {
       environment: 'jsdom',
